1604774e44f5ae9357d50df99e99419fbb7179bb
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2026-05-31 14:09:00 +0900

    Initial Commit

---
+++ PokePhysics/PokePhysicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokePhysicsApp.swift
+//  PokePhysics
+//
+//  Created by 茂木史明 on 2026/05/31.
+//
+
+import SwiftUI
+
+@main
+struct PokePhysicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
